4ef8c61df4b94fdb8e533deec8b50fb1635c3c2d
IbraAguero <ibrahimaguero@gmail.com>
2024-09-14 22:45:05 -0300

    feat: add animation with framter motion

--- src/Chatbot.tsx
+++ src/Chatbot.tsx
@@ -1,4 +1,8 @@
 import { useEffect, useRef, useState } from "react";
+import ChatbotIcon from "./ChatbotIcon";
+import CloseIcon from "./CloseIcon";
+import MessageIcon from "./MessageIcon";
+import { AnimatePresence, motion } from "framer-motion";
 
 const FLOWS = {
   bienvenida: {
@@ -86,6 +90,7 @@ function Chatbot({ initialFlow }) {
   const [flow, setFlow] = useState<string | null>(initialFlow);
   const [messages, setMessages] = useState<Message[]>([]);
   const [input, setInput] = useState<string>("");
+  const [showChat, setShowChat] = useState<boolean>(false);
   const [showOptions, setShowOptions] = useState<boolean>(false);
 
   const messagesEndRef = useRef<HTMLDivElement>(null);
@@ -104,7 +109,7 @@ function Chatbot({ initialFlow }) {
 
   useEffect(() => {
     addMessagesWithDelay(FLOWS[initialFlow].message);
-  }, []);
+  }, [initialFlow]);
 
   const handleClick = (option: string) => {
     const nextStep = FLOWS[flow].nextFlows[option];
@@ -164,78 +169,84 @@ function Chatbot({ initialFlow }) {
 
   return (
     <main className="grid place-content-center h-screen">
-      <section className="w-[350px] h-[500px] rounded-xl shadow-[0px_20px_60px_rgba(0,0,0,_0.5)] flex flex-col">
-        <h1 className="p-4 px-6 rounded-t-xl font-bold text-white text-xl bg-gradient-to-r from-cyan-500 to-blue-500">
-          Bot Ibra
-        </h1>
-        <div className="py-4 px-4 flex-grow overflow-y-auto rounded-md flex flex-col gap-2">
-          {messages.map((message, index) => (
-            <div
-              key={index}
-              className={`flex ${
-                message.sender === "bot" ? "justify-start" : "justify-end"
-              }`}
-            >
-              <div
-                className={`animation ${
-                  message.sender === "bot"
-                    ? "bg-gray-300 text-black rounded-bl-none"
-                    : "bg-blue-500 text-white rounded-br-none my-1.5"
-                } rounded-lg p-2 px-3 shadow-[0px_4px_12px_rgba(0,0,0,_0.3)] text-sm font-normal max-w-[85%] break-words`}
-              >
-                {message.text}
-              </div>
-            </div>
-          ))}
-          {showOptions && (
-            <div className="flex flex-col gap-2">
-              {flow &&
-                FLOWS[flow]?.options?.map((option: string) => (
-                  <button
-                    key={option}
-                    onClick={() => handleClick(option)}
-                    className="animation p-2 px-5 w-fit text-sm font-medium rounded-lg border border-blue-400 text-blue-400 hover:text-white hover:bg-blue-400 transition-colors"
-                  >
-                    {option}
-                  </button>
-                ))}
-            </div>
-          )}
-          <div ref={messagesEndRef}></div>
-        </div>
-        <div>
-          <form
-            onSubmit={(e) => handleSubmit(e)}
-            className="flex gap-2 py-4 border-t-2 border-blue-600 p-3"
+      <AnimatePresence>
+        {showChat && (
+          <motion.section
+            initial={{ opacity: 0, height: "0px" }}
+            animate={{ opacity: 1, height: "500px" }}
+            exit={{ opacity: 0, height: "0px" }}
+            className="fixed right-4 bottom-20 w-[350px] h-[500px] rounded-xl shadow-[0px_20px_60px_rgba(0,0,0,_0.5)] flex flex-col"
           >
-            <input
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              type="text"
-              placeholder="Escribí acá.."
-              className="flex-grow rounded-lg text-sm font-medium px-2 outline-none"
-            />
-            <button>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="icon icon-tabler icons-tabler-outline icon-tabler-circle-chevron-right text-blue-800"
+            <div className="flex items-center justify-between p-4 px-6 rounded-t-xl bg-gradient-to-r from-cyan-500 to-blue-500">
+              <h1 className=" font-bold text-white text-xl ">Bot Ibra</h1>
+            </div>
+            <div className="py-4 px-4 flex-grow overflow-y-auto rounded-md flex flex-col gap-2">
+              {messages.map((message, index) => (
+                <div
+                  key={index}
+                  className={`flex ${
+                    message.sender === "bot" ? "justify-start" : "justify-end"
+                  }`}
+                >
+                  <div
+                    className={`animation ${
+                      message.sender === "bot"
+                        ? "bg-gray-300 text-black rounded-bl-none"
+                        : "bg-blue-500 text-white rounded-br-none my-1.5"
+                    } rounded-lg p-2 px-3 shadow-[0px_4px_12px_rgba(0,0,0,_0.3)] text-sm font-normal max-w-[85%] break-words`}
+                  >
+                    {message.text}
+                  </div>
+                </div>
+              ))}
+              {showOptions && (
+                <div className="flex flex-col gap-2">
+                  {flow &&
+                    FLOWS[flow]?.options?.map((option: string) => (
+                      <button
+                        key={option}
+                        onClick={() => handleClick(option)}
+                        className="animation p-2 px-5 w-fit text-sm font-medium rounded-lg border border-blue-400 text-blue-400 hover:text-white hover:bg-blue-400 transition-colors"
+                      >
+                        {option}
+                      </button>
+                    ))}
+                </div>
+              )}
+              <div ref={messagesEndRef}></div>
+            </div>
+            <div>
+              <form
+                onSubmit={(e) => handleSubmit(e)}
+                className="flex gap-2 py-4 border-t-2 border-blue-600 p-3"
               >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M11 9l3 3l-3 3" />
-                <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0z" />
-              </svg>
-            </button>
-          </form>
-        </div>
-      </section>
+                <input
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+                  type="text"
+                  placeholder="Escribí acá.."
+                  className="flex-grow rounded-lg text-sm font-medium px-2 outline-none"
+                />
+                <button>
+                  <MessageIcon />
+                </button>
+              </form>
+            </div>
+          </motion.section>
+        )}
+      </AnimatePresence>
+      <button
+        onClick={() => setShowChat(!showChat)}
+        className="fixed bottom-4 right-4 bg-blue-500 w-12 h-12 rounded-full grid place-content-center text-white transition-colors"
+      >
+        <motion.div
+          initial={{ rotate: 0 }}
+          animate={{ rotate: showChat ? 180 : 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          {showChat ? <CloseIcon /> : <ChatbotIcon />}
+        </motion.div>
+      </button>
     </main>
   );
 }
